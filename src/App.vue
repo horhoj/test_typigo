122a68699776718032f101e23493aff91fe361b0
Vasiliy Zadoya <cool29@yandex.ru>
2025-04-08 23:59:24 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="main">
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-grow: 1;
+  max-width: 1280px;
+  margin: 0 auto;
+  width: 100%;
+  background-color: white;
+  padding: 15px;
+}
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="app">App: VUE</div>
+</template>
+
+<style lang="css" scoped>
+.app {
+}
+</style>
